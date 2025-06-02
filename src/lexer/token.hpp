c65b89cbaf63110616cfab206125b04179da9d33
Gazach <gamerzcraft37@gmail.com>
2025-06-02 21:32:48 +0700

    add token

---
+++ src/lexer/token.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include <string>
+
+
+enum class TokenType {
+    // KEYWORD
+    LET,
+    IDENTIFIER,
+    NUMBER,
+    PRINT,
+
+    // Math Operation
+    PLUS,
+    MINUS,
+    STAR,
+    SLASH,
+
+    // Single character
+    EQUAL,
+    SEMICOLON,
+    LPAREN,
+    RPAREN,
+
+    // 
+    END_OF_FILE,
+    UNKNOWN
+};
+
+struct Token {
+    TokenType type;
+    std::string value;
+
+    Token(TokenType type, const std::string& value = "")
+        : type(type), value(value) {}
+};
